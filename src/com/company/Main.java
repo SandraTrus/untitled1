e202c7257764ee9d425fc68d68fa3afd40348a9a
unknown <you@example.com>
2021-06-18 20:40:46 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int firstNumber;
+	    int secondNumber;
+	    firstNumber = 5;
+	    secondNumber = 6;
+
+
+
+
+
+		String firstString = "Chips";
+		String secondString = "Chicken";
+
+		System.out.println(firstString);
+
+		firstString = "Grape Jusice";
+
+    }
+}
